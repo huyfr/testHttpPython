747b1c8872d4ab96c1cbaceeabe959aac58fbc04
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-13 17:34:10 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests as requests
+
+HOST = 'http://127.0.0.1:9501'
+API = '/api?action=sendMessage'
+USER_NAME = 'admin'
+PASSWORD = 'admin'
+RECIPIENT = '0858358088'
+MESSAGE_BODY = 'Hello world from python 3'
+MESSAGE_SUCCESS = 'Message accepted for delivery'
+
+
+def compose_request(host, api, user_name, password, recipient, message_body):
+    req = ''
+    try:
+        req = host + api +\
+              '&username=' + requests.utils.quote(user_name) + \
+              '&password=' + requests.utils.quote(password) +\
+              '&recipient=' + requests.utils.quote(recipient) + \
+              '&messagedata=' + requests.utils.quote(message_body)
+    except Exception as ex:
+        print(f'Error at function compose_request with message: ', ex)
+    return req
+
+
+def send_request(req):
+    result = False
+    try:
+        if req != '':
+            resp = requests.post(req)
+            str_content = resp.content.decode('utf-8')
+            if resp.status_code == 200 and MESSAGE_SUCCESS in str_content:
+                result = True
+                print('Send SMS successful!')
+            else:
+                print('Fail while send SMS')
+    except Exception as ex:
+        print(f'Error at function send_request with message: ', ex)
+    return result
+
+
+if __name__ == '__main__':
+    request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY)
+    response = send_request(request)
