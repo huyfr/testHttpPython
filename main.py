1f29ef0e610ae7f3c379cccaf7ce72670acee394
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-17 11:51:45 +0700

    initial ozeki_10

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+import time
+
 import requests as requests
 
-HOST = 'http://127.0.0.1:9501'
-API = '/api?action=sendMessage'
-USER_NAME = 'admin'
-PASSWORD = 'admin'
+HOST = 'http://127.0.0.1:9509'
+API = '/api?action=sendmessage'
+USER_NAME = 'huy'
+PASSWORD = 'huy'
 RECIPIENT = '0858358088'
-MESSAGE_BODY = 'Hello world from python 3'
+MESSAGE_BODY = 'Hello world from python 3 - '
 MESSAGE_SUCCESS = 'Message accepted for delivery'
 
 
-def compose_request(host, api, user_name, password, recipient, message_body):
+def compose_request(host, api, user_name, password, recipient, message_body, count):
     req = ''
     try:
         req = host + api +\
               '&username=' + requests.utils.quote(user_name) + \
               '&password=' + requests.utils.quote(password) +\
               '&recipient=' + requests.utils.quote(recipient) + \
-              '&messagedata=' + requests.utils.quote(message_body)
+              '&messagedata=' + requests.utils.quote(message_body + str(count))
     except Exception as ex:
         print(f'Error at function compose_request with message: ', ex)
     return req
@@ -26,7 +28,7 @@ def send_request(req):
     result = False
     try:
         if req != '':
-            resp = requests.post(req)
+            resp = requests.get(req)
             str_content = resp.content.decode('utf-8')
             if resp.status_code == 200 and MESSAGE_SUCCESS in str_content:
                 result = True
@@ -39,5 +41,7 @@ def send_request(req):
 
 
 if __name__ == '__main__':
-    request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY)
+    print(f'Start time: ', time.time())
+    request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY, 1)
     response = send_request(request)
+    print(f'End time: ', time.time())
