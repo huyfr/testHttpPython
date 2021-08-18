7bbfd623b2ba7ad55b97755d9c88e8868a746577
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-18 15:52:17 +0700

    test send 1m messages

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ API = '/api?action=sendMessage'
 USER_NAME = 'admin'
 PASSWORD = 'admin'
 RECIPIENT = '0858358088'
-MESSAGE_BODY = 'Hello world from python 3'
+MESSAGE_BODY = 'Hello world from python 3 - '
 MESSAGE_SUCCESS = 'Message accepted for delivery'
 
 
-def compose_request(host, api, user_name, password, recipient, message_body):
+def compose_request(host, api, user_name, password, recipient, message_body, count):
     req = ''
     try:
         req = host + api +\
               '&username=' + requests.utils.quote(user_name) + \
               '&password=' + requests.utils.quote(password) +\
               '&recipient=' + requests.utils.quote(recipient) + \
-              '&messagedata=' + requests.utils.quote(message_body)
+              '&messagedata=' + requests.utils.quote(message_body + str(count))
     except Exception as ex:
         print(f'Error at function compose_request with message: ', ex)
     return req
@@ -30,7 +30,7 @@ def send_request(req):
             str_content = resp.content.decode('utf-8')
             if resp.status_code == 200 and MESSAGE_SUCCESS in str_content:
                 result = True
-                print('Send SMS successful!')
+                # print('Send SMS successful!')
             else:
                 print('Fail while send SMS')
     except Exception as ex:
@@ -39,5 +39,6 @@ def send_request(req):
 
 
 if __name__ == '__main__':
-    request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY)
-    response = send_request(request)
+    for i in range(1000000):
+        request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY, i)
+        response = send_request(request)
