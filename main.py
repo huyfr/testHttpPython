01e648e87524ba905c5ad49ae32ccc9ecfa84262
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-19 16:09:42 +0700

    test send 1m messages

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from smpplib import consts
 import tqdm
 
 if __name__ == '__main__':
-    for i in tqdm.tqdm(range(10)):
+    for i in tqdm.tqdm(range(10000000)):
         client = None
         try:
             client = smpplib.client.Client(host='192.168.1.100', port=9550)
             client.connect()
             try:
-                client.bind_transmitter(system_id='testSmpp', password='testSmpp')
+                client.bind_transmitter(system_id='fb', password='fb')
                 message_bytes = bytes('test message send smpp by python3 - ' + str(i), 'utf-8')
                 client.send_message(source_addr_ton=smpplib.consts.SMPP_TON_INTL,
-                                    source_addr='0858358088',
+                                    source_addr='3332222332266666655',
                                     dest_addr_ton=smpplib.consts.SMPP_TON_INTL,
-                                    destination_addr='9535134654',
+                                    destination_addr='66699993355444',
+                                    registered_delivery=True,
                                     short_message=message_bytes)
             except Exception as ex:
                 print(ex)
