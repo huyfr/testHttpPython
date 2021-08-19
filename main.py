68ed1aa0e90f8ae25b5ac28142c7cb2a55e2cf20
huyfr <61008601+huyfr@users.noreply.github.com>
2021-08-19 13:49:27 +0700

    initial send message by smpp

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
-import requests as requests
+import smpplib
+from smpplib import consts
 import tqdm
 
-HOST = 'http://127.0.0.1:9509'
-API = '/api?action=sendmessage'
-USER_NAME = 'huy'
-PASSWORD = 'huy'
-RECIPIENT = '0858358088'
-MESSAGE_BODY = 'Hello world from python 3 - '
-MESSAGE_SUCCESS = 'Message accepted for delivery'
-
-
-def compose_request(host, api, user_name, password, recipient, message_body, count):
-    req = ''
-    try:
-        req = host + api +\
-              '&username=' + requests.utils.quote(user_name) + \
-              '&password=' + requests.utils.quote(password) +\
-              '&recipient=' + requests.utils.quote(recipient) + \
-              '&messagedata=' + requests.utils.quote(message_body + str(count))
-    except Exception as ex:
-        print(f'Error at function compose_request with message: ', ex)
-    return req
-
-
-def send_request(req):
-    result = False
-    try:
-        if req != '':
-            resp = requests.get(req)
-            str_content = resp.content.decode('utf-8')
-            if resp.status_code == 200 and MESSAGE_SUCCESS in str_content:
-                result = True
-                # print('Send SMS successful!')
-            else:
-                print('Fail while send SMS')
-    except Exception as ex:
-        print(f'Error at function send_request with message: ', ex)
-    return result
-
-
 if __name__ == '__main__':
-    for i in tqdm.tqdm(range(1000)):
-        request = compose_request(HOST, API, USER_NAME, PASSWORD, RECIPIENT, MESSAGE_BODY, 1)
-        response = send_request(request)
+    for i in tqdm.tqdm(range(10)):
+        client = None
+        try:
+            client = smpplib.client.Client(host='192.168.1.100', port=9550)
+            client.connect()
+            try:
+                client.bind_transmitter(system_id='testSmpp', password='testSmpp')
+                message_bytes = bytes('test message send smpp by python3 - ' + str(i), 'utf-8')
+                client.send_message(source_addr_ton=smpplib.consts.SMPP_TON_INTL,
+                                    source_addr='0858358088',
+                                    dest_addr_ton=smpplib.consts.SMPP_TON_INTL,
+                                    destination_addr='9535134654',
+                                    short_message=message_bytes)
+            except Exception as ex:
+                print(ex)
+            finally:
+                if client.state in [smpplib.consts.SMPP_CLIENT_STATE_BOUND_TX]:
+                    try:
+                        client.unbind()
+                    except smpplib.exceptions.UnknownCommandError as ex:
+                        try:
+                            client.unbind()
+                        except smpplib.exceptions.PDUError as ex:
+                            pass
+        finally:
+            if client:
+                client.disconnect()
